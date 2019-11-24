ab98113c1788d8137f3e9547e62125e2cab3fcd4
Melvin Carvalho <melvincarvalho@gmail.com>
2019-11-24 12:37:42 +0100

    add w3c

---
+++ w3c/index.mm
@@ -0,0 +1,93 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="W3C" FOLDED="false" ID="ID_237352286" CREATED="1570576809840" MODIFIED="1574595403620" ICON_SIZE="16.0 pt" LINK="../index.mm" STYLE="oval">
+<icon BUILTIN="wizard"/>
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="16" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Web" POSITION="right" ID="ID_176155545" CREATED="1572063754537" MODIFIED="1574595387482" HGAP_QUANTITY="-72.99999740719801 pt" VSHIFT_QUANTITY="-74.24999778717762 pt" COLOR="#006633">
+<icon BUILTIN="list"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="General" POSITION="right" ID="ID_527312312" CREATED="1572063758657" MODIFIED="1574595403619" HGAP_QUANTITY="50.74999890476468 pt" VSHIFT_QUANTITY="-10.499999687075622 pt" COLOR="#000099">
+<icon BUILTIN="launch"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#00ff00"/>
+<node TEXT="Native File System" ID="ID_1944964332" CREATED="1573041667364" MODIFIED="1573041678745" LINK="https://wicg.github.io/native-file-system/"/>
+<node TEXT="Web Share API" ID="ID_131281787" CREATED="1573042137566" MODIFIED="1573042142561" LINK="https://w3c.github.io/web-share/"/>
+<node TEXT="Credential Management Level 1" ID="ID_1415592675" CREATED="1573042176019" MODIFIED="1573042185242" LINK="https://www.w3.org/TR/credential-management-1/"/>
+</node>
+<node TEXT="URIs" POSITION="right" ID="ID_1810859973" CREATED="1572063767900" MODIFIED="1574595391056" HGAP_QUANTITY="-71.49999745190152 pt" VSHIFT_QUANTITY="63.749998100102005 pt" COLOR="#666600">
+<icon BUILTIN="executable"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#ff00ff"/>
+<node TEXT="DID" ID="ID_236478047" CREATED="1574595405731" MODIFIED="1574595407922"/>
+</node>
+<node TEXT="Links" POSITION="left" ID="ID_777477932" CREATED="1572063875737" MODIFIED="1572064427848" COLOR="#660000">
+<icon BUILTIN="attach"/>
+<font SIZE="12" BOLD="true"/>
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
